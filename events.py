fce248bb8bcf1165388c636a5669a439de759f00
Sacul <lucastanhongyi@gmail.com>
2025-08-02 00:39:13 +0800

    Remove old event and add a new one, and some minor changes

--- events.py
+++ events.py
@@ -9,16 +9,19 @@ import os
 import asqlite
 import random
 from random import randint
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from cats import EventBot
 
-to_pick_from : list = ["cat", "none", "othing"]
+to_pick_from : tuple = ("cat", "no", "nope")
 
 def calc_guess(time_taken : float) -> int:
     earnings = 200 - (time_taken / 900) * 100
     return int(earnings)
 
 class GuessView(discord.ui.View):
-    def __init__(self, start_time : float, bot:commands.Bot):
+    def __init__(self, start_time : float, bot: EventBot):
         super().__init__(timeout=600)
         random_choice = tuple(random.sample(to_pick_from, 3))
         self.first, self.second, self.third = random_choice
@@ -27,16 +30,16 @@ class GuessView(discord.ui.View):
         self.messsage :discord.Message | None = None
         self.users :list[int] = []
 
-
     async def on_timeout(self):
         for item in self.children:
             item.disabled = True
-        await self.message.edit(view=self)
+        await self.messsage.edit(view=self)
+        
     @discord.ui.button(label="1", style=discord.ButtonStyle.secondary, custom_id="button1")
     async def button1(self, interaction:discord.Interaction, button:discord.ui.Button):
         await interaction.response.defer(ephemeral=True)
         if interaction.user.id in self.users:
-            return await interaction.followup.send(f"You can only try once! This is your second attempt :(")
+            return await interaction.followup.send(f"You can only try once! This is your second attempt :(", ephemeral=True)
         if self.first == "cat":
             time_taken = time.perf_counter() - self.start_time
             for child in self.children:
@@ -50,23 +53,21 @@ class GuessView(discord.ui.View):
             embed.set_author(name=f"@{interaction.user} won!", icon_url=interaction.user.display_avatar.url)
             await interaction.followup.send(embed=embed)
             async with self.bot.economy_pool.acquire() as conn:
-                row = await conn.execute('''SELECT money FROM economydb WHERE user_id = ?''',(interaction.user.id,))
-                result = await row.fetchone()
-                amount = result["money"] if result is not None else 0
-                new_amount = amount + earned
                 await conn.execute('''INSERT INTO economydb (user_id, money) VALUES (?, ?)
-                                   ON CONFLICT(user_id) DO UPDATE SET money = excluded.money''',
-                                   (interaction.user.id, new_amount))
+                                   ON CONFLICT(user_id) DO UPDATE SET money = money + excluded.money''',
+                                   (interaction.user.id, earned))
                 await conn.commit()
         else:
             embed = discord.Embed(title="Wrong one!",
-                                  description="Try again, don't give up!",
+                                  description="Sadly you can only try once",
                                   color=discord.Color.brand_red())
             self.users.append(interaction.user.id)
             await interaction.followup.send(embed=embed, ephemeral=True)
     @discord.ui.button(label="2", style=discord.ButtonStyle.secondary, custom_id="button2")
     async def button2(self, interaction:discord.Interaction, button:discord.ui.Button):
         await interaction.response.defer(ephemeral=True)
+        if interaction.user.id in self.users:
+            return await interaction.followup.send(f"You can only try once! This is your second attempt :(", ephemeral=True)
         if self.second == "cat":
             time_taken = time.perf_counter() - self.start_time
             for child in self.children:
@@ -80,23 +81,21 @@ class GuessView(discord.ui.View):
             embed.set_author(name=f"@{interaction.user} won!", icon_url=interaction.user.display_avatar.url)
             await interaction.followup.send(embed=embed)
             async with self.bot.economy_pool.acquire() as conn:
-                row = await conn.execute('''SELECT money FROM economydb WHERE user_id = ?''',(interaction.user.id,))
-                result = await row.fetchone()
-                amount = result["money"] if result is not None else 0
-                new_amount = amount + earned
                 await conn.execute('''INSERT INTO economydb (user_id, money) VALUES (?, ?)
-                                   ON CONFLICT(user_id) DO UPDATE SET money = excluded.money''',
-                                   (interaction.user.id, new_amount))
+                                   ON CONFLICT(user_id) DO UPDATE SET money = money + excluded.money''',
+                                   (interaction.user.id, earned))
                 await conn.commit()
         else:
             embed = discord.Embed(title="Wrong one!",
-                                  description="Try again, don't give up!",
+                                  description="Sadly you can only try once",
                                   color=discord.Color.brand_red())
             self.users.append(interaction.user.id)
             await interaction.followup.send(embed=embed, ephemeral=True)
     @discord.ui.button(label="3", style=discord.ButtonStyle.secondary, custom_id="button3")
     async def button3(self, interaction:discord.Interaction, button:discord.ui.Button):
         await interaction.response.defer(ephemeral=True)
+        if interaction.user.id in self.users:
+            return await interaction.followup.send(f"You can only try once! This is your second attempt :(", ephemeral=True)
         if self.third == "cat":
             time_taken = time.perf_counter() - self.start_time
             for child in self.children:
@@ -110,21 +109,93 @@ class GuessView(discord.ui.View):
             embed.set_author(name=f"@{interaction.user} won!", icon_url=interaction.user.display_avatar.url)
             await interaction.followup.send(embed=embed)
             async with self.bot.economy_pool.acquire() as conn:
-                row = await conn.execute('''SELECT money FROM economydb WHERE user_id = ?''',(interaction.user.id,))
-                result = await row.fetchone()
-                amount = result["money"] if result is not None else 0
-                new_amount = amount + earned
                 await conn.execute('''INSERT INTO economydb (user_id, money) VALUES (?, ?)
-                                   ON CONFLICT(user_id) DO UPDATE SET money = excluded.money''',
-                                   (interaction.user.id, new_amount))
+                                   ON CONFLICT(user_id) DO UPDATE SET money = money + excluded.money''',
+                                   (interaction.user.id, earned))
                 await conn.commit()
         else:
-            embed = discord.Embed(title="Wrong one!",
-                                  description="Try again, don't give up!",
-                                  color=discord.Color.brand_red())
-            
-            self.users.append(interaction.user.id)
-            await interaction.followup.send(embed=embed, ephemeral=True)
+            try:
+                embed = discord.Embed(title="Wrong one!",
+                                      description="Sadly you can only try once",
+                                      color=discord.Color.brand_red())
+                self.users.append(interaction.user.id)
+                await interaction.followup.send(embed=embed, ephemeral=True)
+            except Exception as e:
+                print(e)
+
+class RPCView(discord.ui.View):
+    def __init__(self, bot : EventBot):
+        super().__init__(timeout=600)
+        self.add_item(RPCSelect(bot))
+        self.message : discord.Message | None = None
+
+    async def on_timeout(self):
+        for item in self.children:
+            item.disabled = True
+        await self.message.edit(view=self)
+
+class RPCSelect(discord.ui.Select):
+    def __init__(self, bot:EventBot):
+        options = [discord.SelectOption(label="Rock", value="rock", emoji="🪨"),
+                   discord.SelectOption(label="Paper", value="paper", emoji="📃"),
+                   discord.SelectOption(label="Scissors", value="scissors", emoji="✂️")]
+        super().__init__(custom_id="RPCSelect", placeholder="Choose Rock, Paper or Scissors... ", min_values=1, max_values=1, options=options)
+        self.bot = bot
+    
+    def rps_winner(self, choice1: str, choice2: str) -> str:
+        win_map = {
+            "Rock": "Scissors",
+            "Scissors": "Paper",
+            "Paper": "Rock"
+        }
+
+        if choice1 == choice2:
+            return  "Tie"
+        elif win_map[choice1] == choice2:
+            return "True"
+        else:
+            return "False"
+        
+    async def callback(self, interaction:discord.Interaction):
+        value = self.values[0]
+        async with self.bot.economy_pool.acquire() as conn:
+            row = await conn.execute('''SELECT money FROM economydb WHERE user_id = ?''',(interaction.user.id,))
+            result = await row.fetchone()
+            if result is not None:
+                amount = result["money"]
+            else:
+                amount = 0
+            rand_ans = random.choice(["Rock", "Paper", "Scissors"])
+            eraned_or_lose = randint(80, 150)
+            if rand_ans != value:
+                result = self.rps_winner(value, rand_ans)
+                if result == "True":
+                    total = amount + eraned_or_lose
+                else:
+                    total = max(amount - eraned_or_lose, 0)
+                await conn.execute('''INSERT INTO economydb (user_id, money) VALUES (?, ?)
+                                ON CONFLICT(user_id) DO UPDATE SET money = excluded.money''', (interaction.user.id, total))
+                await conn.commit()
+        if result == "True":
+            embed = discord.Embed(title=f"You won!",
+                                description=f"- You earned 🪙**{eraned_or_lose} catbucks.** Well done!\
+                                    \n- You chose: `{eraned_or_lose}` | The bot: `{rand_ans}`",
+                                color=discord.Color.brand_green())
+            embed.set_author(name=f"@{interaction.user}", icon_url=interaction.user.display_avatar.url)
+            embed.set_thumbnail(url=interaction.user.display_avatar.url)
+        elif result == "False":
+            embed = discord.Embed(title=f"You lost D:",
+                                description=f"- You let 🪙**{eraned_or_lose} catbucks go down the drain...** \
+                                    \n- You chose: `{eraned_or_lose}` | The bot: `{rand_ans}`",
+                                color=discord.Color.brand_red())
+            embed.set_author(name=f"@{interaction.user}", icon_url=interaction.user.display_avatar.url)
+            embed.set_thumbnail(url=interaction.user.display_avatar.url)
+        else:
+            embed = discord.Embed(title=f"It's a tie!",
+                                description=f"- You chose: `{eraned_or_lose}` | The bot: `{rand_ans}`")
+            embed.set_author(name=f"@{interaction.user}", icon_url=interaction.user.display_avatar.url)
+            embed.set_thumbnail(url=interaction.user.display_avatar.url)
+        await interaction.followup.send(embed=embed)
 
 
 class EventCog(commands.Cog):
@@ -136,51 +207,20 @@ class EventCog(commands.Cog):
     async def cog_load(self):
         self.task1.start()
         self.task2.start()
-
-    def calculate_meow(self, time_seconds) -> int:
-        time_seconds = max(0, min(300, time_seconds))
-        score = 100 + ((300 - time_seconds) / 300) * 100
-        return round(score)
-
+    
     @tasks.loop(hours=1)
     async def task1(self) -> None:
         self.count1 += 1
-        if self.count1 % 2 == 0:
+        if self.count1 % 2 ==0:
             return
         channel = self.bot.get_channel(1319213192873775218) or await self.bot.fetch_channel(1319213192873775218)
+        view= RPCView(self.bot)
         embed = discord.Embed(title="Minigames!",
-                              description=f"- First one to type `meow` wins!\
-                                \n- Starts in: <t:{int(time.time()) + 2}:R>",
-                                color=0xff8a42)
-        await channel.send(embed=embed)
-        await asyncio.sleep(2.0)
-        start_time = time.perf_counter()
-        def check(msg :discord.Message):
-            return msg.channel == channel and msg.content.lower() == "meow"
-        try:
-            message = await self.bot.wait_for("message", check=check, timeout=300)
-        except asyncio.TimeoutError:
-            await channel.send(f"The game has ended, sadly no one won :(")
-            return
-        else:
-            time_taken = time.perf_counter() - start_time
-            earned = self.calculate_meow(time_taken)
-            embed = discord.Embed(title=f"",
-                                description=f"- You earned `🪙{earned}`\n- You took: `{time_taken:.2f}s`",
-                                timestamp=discord.utils.utcnow(),
-                                color=discord.Color.brand_green())
-            embed.set_author(name=f"@{message.author} won!", icon_url=message.author.display_avatar.url)
-            await message.reply(embed=embed)
-            async with self.bot.economy_pool.acquire() as conn:
-                row = await conn.execute('''SELECT money FROM economydb WHERE user_id =?''',
-                                         (message.author.id,))
-                result = await row.fetchone()
-                amount = result["money"] if result is not None else 0
-                new_amount = amount + earned
-                await conn.execute('''INSERT INTO economydb (user_id, money) VALUES (?, ?)
-                                   ON CONFLICT(user_id) DO UPDATE SET money = excluded.money''',
-                                   (message.author.id, new_amount))
-        
+                              description=f"- Play **Rock, Paper, Scissors**!\
+                                \n- Ends in: <t:{int(time.time()) + 600}:R>",
+                              color=0xff8a42,)
+        message = await channel.send(embed=embed, view=view)
+        view.message = message
 
     @tasks.loop(hours=1)
     async def task2(self) -> None:
@@ -194,7 +234,7 @@ class EventCog(commands.Cog):
                                 \n- Ends in: <t:{int(time.time()) + 600}:R>",
                               color=0xff8a42,)
         message = await channel.send(embed=embed,view=view)
-        view.message = message #Assigns self.message to the message sent
+        view.messsage = message #Assigns self.message to the message sent
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(EventCog(bot))
