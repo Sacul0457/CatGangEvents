82f19fd247d6c716ddf5798610feb4336067b4ff
Sacul <lucastanhongyi@gmail.com>
2025-08-02 00:57:40 +0800

    Update logs.py

--- logs.py
+++ logs.py
@@ -5,11 +5,8 @@ import time
 from datetime import timedelta
 import datetime
 import os
-import asqlite
-import random
-from random import randint
-
-
+import re
+pattern = re.compile(r"[^\u0000-\u007F\u02B0-\u02FF\u1F300-\u1F6FF\s.,!?\"'@#$%^&*()+=/\[\]{}:;<>|`~\\\-]")
 class LogsCog(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
@@ -17,6 +14,8 @@ class LogsCog(commands.Cog):
     @commands.Cog.listener()    
     async def on_member_join(self, member:discord.Member):
         channel = member.guild.get_channel(1382321640507052052)
+        if not channel:
+            return
         embed = discord.Embed(title="Welcome to the CatGang Server!",
                               description="- You are now officially a cat gangster! <a:error_cat:1339247313310191740>",
                               color=0xfff792,
@@ -24,9 +23,13 @@ class LogsCog(commands.Cog):
         embed.set_author(name=f"@{member}", icon_url=member.display_avatar.url)
         embed.set_thumbnail(url=member.guild.icon.url)
         await channel.send(f"{member.mention}", embed=embed)
+
+    
     @commands.Cog.listener()
     async def on_member_remove(self, member:discord.Member):
         channel = member.guild.get_channel(1382321640507052052)
+        if not channel:
+            return
         embed = discord.Embed(title="You left the CatGang Server! Why?",
                               description="- Now you're not a cat ganster anymore :( <a:scream_cat:1339243936924962826>",
                               color=0xb82407,
